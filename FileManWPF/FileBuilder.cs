971535b5e7cdff4d17516168da151f79c653dcf8
shred14 <donovanhoffman@yahoo.com.au>
2014-06-12 10:29:56 +1200

    Basic functionality works now
    Class use has been refined a bit
    The MVVM in use could take advantage of more events though
    TODO:
    Result File Preview still needs to be implemented
    GUI Design needs to be refined a bit
    Usability could definitely use some improvement

--- FileManWPF/FileBuilder.cs
+++ FileManWPF/FileBuilder.cs
@@ -4,71 +4,135 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Windows;
+using System.Collections.ObjectModel;
 
 namespace FileManWPF {
     class FileBuilder {
         List<String> _finalFileData;
         Dictionary<String,List<int>> _sourceFiles;// the key is the path, the value is the column exclusion list
-        List<int> idFileA;
-        List<int> idFileB;
         RawFile _fileA, _fileB;
         char _separator;
+        ObservableCollection<object> _fileADataPreview, _fileBDataPreview;
 
         public FileBuilder() {
             _finalFileData = new List<string>();
             _sourceFiles = new Dictionary<string, List<int>>();
             _separator = ',';
+            _fileADataPreview = new ObservableCollection<object>();
+            _fileBDataPreview = new ObservableCollection<object>();
         }
 
-        public FileBuilder(char sep, string fileA, string fileB){
+        public FileBuilder(char sep){
             _finalFileData = new List<string>();
             _sourceFiles = new Dictionary<string, List<int>>();
             _separator = sep;
-            _fileA = new RawFile(fileA);
-            _fileB = new RawFile(fileB);
+            _fileADataPreview = new ObservableCollection<object>();
+            _fileBDataPreview = new ObservableCollection<object>();
         }
 
         public RawFile FileA {
             get { return _fileA; }
-            set { _fileA = value; }
+            set { _fileA = value;
+                   _fileADataPreview = _fileA.GetDataPreview(5);
+                }
         }
 
         public RawFile FileB {
             get { return _fileB; }
-            set { _fileB = value; }
+            set { _fileB = value;
+                _fileBDataPreview = _fileB.GetDataPreview(5);
+            }
         }
+
+        public ObservableCollection<object> FileAPreviewData {
+            get { return _fileADataPreview;}
+        }
+
+        public ObservableCollection<object> FileBPreviewData{
+            get { return _fileBDataPreview;}
+        }
+
+        public void ManageExclusionChange(string content, bool include) {
+
+
+            
+            char file = content[content.Length - 1];//getting the last character to determine if fileA or fileB
+            content = content.Remove(content.Length - 2);//removing so that header can be matched properly
+
+            if (file == 'A') {
+                this.FileA.Headers[content] = include;
+            }
+            else if (file == 'B') {
+                this.FileB.Headers[content] = include;
+            }
+            else {
+                MessageBox.Show("There was a problem in changing the exclusion");
+            }
+        }
+        
 
         //chunkSize says how many lines should be processed of the files, to avoid memory problems if sourceFiles are too big
         public void ProcessData(int chunkSize, string destination) {
             try {
-                StreamReader readerFileA = _fileA.GetReader();
-                StreamReader readerFileB = _fileB.GetReader();
+                TextReader readerFileA = _fileA.GetReader();
+                
 
-                while (!readerFileA.EndOfStream) {
+                if(_fileA.HasHeader)
+                    readerFileA.ReadLine();
+
+                List<string> headers = _fileA.GetResultHeaders();
+                headers.AddRange(_fileB.GetResultHeaders());
+                string line = "";
+
+                foreach (string s in headers) {
+                    line += s + ",";
+                }
+
+                line = line.Remove(line.Length - 1);
+
+                    
+                StreamWriter writer = new StreamWriter(destination, true);
+                writer.WriteLine(line);
+                writer.Flush();
+                writer.Close();
+
+
+                while (readerFileA.Peek() != -1) {
+                    
                     string lineA = readerFileA.ReadLine();
-                    string idA = _fileA.GenerateID(lineA);
 
-                    while (!readerFileB.EndOfStream) {
+                    string idA = _fileA.GenerateID(lineA);
+                    TextReader readerFileB = _fileB.GetReader();
+                    
+                    if (_fileB.HasHeader)
+                        readerFileB.ReadLine();
+
+                    while (readerFileB.Peek() != -1) {
                         string lineB = readerFileB.ReadLine();
                         string idB = _fileB.GenerateID(lineB);
 
                         if (idA == idB) {
                             _finalFileData.Add(_fileA.GenerateResult(lineA) + _fileA.Separator + _fileB.GenerateResult(lineB));
 
-                            if (_finalFileData.Count == chunkSize) {
+                            if (_finalFileData.Count >= chunkSize) {
                                 writeResults(destination);
                             }
                         }
                     }
                 }
+                writeResults(destination);
+                MessageBox.Show("File has been written");
             }
             catch (IOException ex) {
+                MessageBox.Show("I have an exception");
                 System.Console.Error.WriteLine("IO Error occured in " + ex.Source + Environment.NewLine
                     + ex.Message + Environment.NewLine +
                     "Stack Trace:" + Environment.NewLine
                     + ex.StackTrace);
             }
         }
+
 
         private void writeResults(string destination) {
             try {
@@ -91,5 +155,20 @@ namespace FileManWPF {
         }
 
 
+
+
+        public void ParseAndSetIDs(string fileAID, string fileBID) {
+            setID(_fileA, fileAID);
+            setID(_fileB, fileBID);
+        }
+
+        private void setID(RawFile file, string p) {
+            String[] data = p.Split(',');
+
+            foreach (String entry in data) {
+                int n = int.Parse(entry);
+                file.IDColumnList.Add(n);
+            }
+        }
     }
 }
--- FileManWPF/RawFile.cs
+++ FileManWPF/RawFile.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Windows;
+using System.Collections.ObjectModel;
 
 namespace FileManWPF {
     class RawFile{
@@ -11,14 +13,18 @@ namespace FileManWPF {
         private List<int> _exclusionList;
         private List<int> _idColumnsList;//the column indices that will be unique identifiers for compilation file(so they can map to each other)
         private char _separator = ',';
-        private List<string> _dataTypes;
+        private Dictionary<string,bool> _headers;
+        private bool _hasHeaders;
 
         #region Constructors
 
-        public RawFile(String path) {
+        public RawFile(String path, bool hasHeaders) {
             _path = path;
             _exclusionList = new List<int>();
             _idColumnsList = new List<int>();
+            _hasHeaders = hasHeaders;
+            _headers = new Dictionary<string, bool>();
+            getHeaders();
         }
 
         #endregion
@@ -32,8 +38,19 @@ namespace FileManWPF {
             set{_separator = value;}
         }
 
+        public Dictionary<String,bool> Headers {
+            get { return _headers; }
+            set { _headers = value; }
+        }
+
+        public bool HasHeader {
+            get { return _hasHeaders; }
+        }
+
         public List<int> ExclusionList {
-            get { return _exclusionList; }
+            get {
+                return _exclusionList;
+            }
             set { _exclusionList = value; }
         }
 
@@ -45,42 +62,11 @@ namespace FileManWPF {
         #endregion
 
 
-        #region Methods to manipulate properties
-
-        public void addIDColumnNumber(int n) {
-            if (n < 0)
-                throw new ArgumentException();
-
-            _idColumnsList.Add(n);
-        }
-
-        public void addExclusion(int n) {
-            if (n < 0)
-                throw new ArgumentException();
-
-            _exclusionList.Add(n);
-        }
-
-        #endregion
-
-
-        public StreamReader GetReader() {
+        public TextReader GetReader() {
             return new StreamReader(_path);
         }
 
-        public List<List<String>> GetSample(int size) {
-            List<List<String>> result = new List<List<string>>();
-            StreamReader reader = GetReader();
-
-            while (!reader.EndOfStream && result.Count != size) {
-                string line = reader.ReadLine();
-                string[] data = line.Split(_separator);
-                result.Add(data.ToList<String>());
-            }
-
-
-            return result;
-        }
+       
 
         public string GenerateID(string s) {
             
@@ -100,17 +86,46 @@ namespace FileManWPF {
 
         }
 
+        public void parseAndSetID(String s) {
+            String[] data = s.Split(',');
+
+            foreach(String entry in data){
+                int n = int.Parse(entry);
+                _idColumnsList.Add(n);
+            }
+        }
+
+        public List<string> GetResultHeaders() {
+            List<string> result = new List<string>();
+            for (int index = 0; index < _headers.Count; index++) {
+                var item = _headers.ElementAt(index);
+                if (item.Value == true) {
+                    result.Add(item.Key);
+                }
+            }
+            return result;
+        }
+
         public string GenerateResult(string s) {
             if (s == null)
                 throw new ArgumentNullException();
             else if (s.Length == 0)
                 throw new ArgumentException();
 
+            List<int> result = new List<int>();
+            for (int index = 0; index < _headers.Count; index++) {
+                var item = _headers.ElementAt(index);
+                if (item.Value == false) {
+                    result.Add(index);
+                }
+                this._exclusionList = result;
+            }
+
             StringBuilder sb = new StringBuilder();
             string[] splitInfo = s.Split(_separator);
 
             for (int i = 0; i < splitInfo.Length; i++) {
-                if (!_exclusionList.Contains(i)) {
+                if (!this.ExclusionList.Contains(i)) {
                     if (sb.Length > 0) {
                         sb.Append(_separator);
                     }
@@ -122,48 +137,58 @@ namespace FileManWPF {
             return sb.ToString();
         }
 
+        private void getHeaders() {
 
-        private List<String> DetermineDataTypes() {
-            List<String> result = new List<String>();
+            if (!_hasHeaders)
+                return;
+            TextReader reader = GetReader();
+            String line = reader.ReadLine();
+            List<String> headers = line.Split(_separator).ToList<String>();
+            headers.ForEach(
+                delegate(String header) {
+                    _headers[header] = true;
+                });
+            reader.Close();
+        }
 
-            try {
-                StreamReader reader = new StreamReader(_path);
+        public List<List<String>> GetPreview(int numOfRecords) {
+            List<List<String>> result = new List<List<String>>();
+            TextReader reader = GetReader();
 
-                while (!reader.EndOfStream) {
-                    String line = reader.ReadLine();
-                    string[] data = line.Split(_separator);
+            if (HasHeader)
+                reader.ReadLine();
 
-                    if (result.Count == 0) {
-                        result = new List<string>(data.Length);
-                    }
+            while (reader.Peek() != -1 && result.Count < numOfRecords) {
+                String line = reader.ReadLine();
+                String[] data = line.Split(_separator);
 
-                    for (int i = 0; i < data.Length; i++) {
-                        string s = data[i];
-                        double attempt = 0.0;
-                        bool isANumber = double.TryParse(s, out attempt);
-
-                        if (!isANumber) {
-                            if (result[i] == "numeric" || result[i] == "") {
-                                result[i] = "nominal";
-                            }
-                        }
-                        else {
-                            if (result[i] == "") {
-                                result[i] = "numeric";
-                            }
-                        }
-                    }
-                }
-                reader.Close();
-            }
-            catch (IOException ex) {
-                System.Console.Error.WriteLine("IO Error occured in " + ex.Source + Environment.NewLine
-                    + ex.Message + Environment.NewLine +
-                    "Stack Trace:" + Environment.NewLine
-                    + ex.StackTrace);
+                result.Add(data.ToList<String>());
             }
 
+            reader.Close();
             return result;
+        }
+
+        public ObservableCollection<object> GetDataPreview(int numOfRecords) {
+            ObservableCollection<object> result = new ObservableCollection<object>();
+
+            List<List<String>> preview = GetPreview(5);
+            List<String> headers = GetHeaders();
+
+            foreach (List<String> line in preview) {
+                dynamic dd = new BindableDynamicDictionary();
+                for (int i = 0; i < headers.Count; i++) {
+                    String header = headers[i];
+                    dd[header] = line[i];
+                }
+
+                result.Add(dd);
+            }
+            return result;
+        }
+
+        public List<String> GetHeaders() {
+            return this.Headers.Keys.ToList();
         }
     }
 }
